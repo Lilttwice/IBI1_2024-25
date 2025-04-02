5f85b13dfc11cc268e1c23a6568159c23e52f308
Lilttwice <tongtong.24@intl.zju.edu.cn>
2025-04-02 15:16:58 +0800

    rewrite largest_intron.py

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -23,3 +23,23 @@ for gt_i in gt_positions:
                 max_length = current_length
 
 print(max_length)
+import re
+
+seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
+
+print("gene sequence：", seq)
+
+# 使用正则表达式查找所有GT和AG的位置
+gt_positions = [m.start() for m in re.finditer(r'GT', seq)]
+ag_positions = [m.start() for m in re.finditer(r'AG', seq)]
+
+max_length = 0
+
+for gt in gt_positions:
+    for ag in ag_positions:
+        if ag >= gt + 1:
+            current_length = ag - gt + 2
+            if current_length > max_length:
+                max_length = current_length
+
+print("Length of the largest intron：", max_length)
