ba9c2aefb26b3c5a334294092fb664708469a228
Lilttwice <tongtong.24@intl.zju.edu.cn>
2025-04-02 19:17:07 +0800

    rewrite largest

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,28 +1,4 @@
-seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
-
-# Identify the lagrest intron
-intron = seq.split('T')
-seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
-
-gt_positions = []
-ag_positions = []
-
-for i in range(len(seq) - 1):
-    if seq[i] == 'G' and seq[i+1] == 'T':
-        gt_positions.append(i)
-    if seq[i] == 'A' and seq[i+1] == 'G':
-        ag_positions.append(i)
-
-max_length = 0
-
-for gt_i in gt_positions:
-    for ag_j in ag_positions:
-        if ag_j >= gt_i + 1:
-            current_length = ag_j - gt_i + 2
-            if current_length > max_length:
-                max_length = current_length
-
-print(max_length)
+# Description: This script is used to find the largest intron length in a gene sequence.
 import re
 
 seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
@@ -42,4 +18,4 @@ for gt in gt_positions:
             if current_length > max_length:
                 max_length = current_length
 
-print("Length of the largest intron：", max_length)
+print("The largest intron length is:", max_length)
